1eee738aeaa4002549165ae3b9fda9f73a152c76
Edgar <edguitian@imagine800.es>
2025-04-15 09:44:59 +0200

    Initial Commit

---
+++ RickMortyCoordinatorTests/RickMortyCoordinatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  RickMortyCoordinatorTests.swift
+//  RickMortyCoordinatorTests
+//
+//  Created by Edgar Guitian Rey on 15/4/25.
+//
+
+import Testing
+
+struct RickMortyCoordinatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ RickMortyCoordinatorUITests/RickMortyCoordinatorUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  RickMortyCoordinatorUITestsLaunchTests.swift
+//  RickMortyCoordinatorUITests
+//
+//  Created by Edgar Guitian Rey on 15/4/25.
+//
+
+import XCTest
+
+final class RickMortyCoordinatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
